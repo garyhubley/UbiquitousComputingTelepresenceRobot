ab467e459855d95e4f2209aff683aa129f42737c
Gary Hubley <gary.hubley@dal.ca>
2017-07-04 16:27:06 -0300

    Added the TeleBot Folder back to be tracked.

---
+++ TeleBotGUI/TeleBotGUI/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace TeleBotGUI
+{
+    partial class TeleBot
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TeleBot));
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.UpButton = new System.Windows.Forms.Button();
+            this.RightButton = new System.Windows.Forms.Button();
+            this.DownButton = new System.Windows.Forms.Button();
+            this.QuestionButton = new System.Windows.Forms.Button();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.LeftButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LeftButton.Location = new System.Drawing.Point(65, 109);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(40, 40);
+            this.LeftButton.TabIndex = 0;
+            this.LeftButton.Text = "←";
+            this.LeftButton.UseVisualStyleBackColor = true;
+            this.LeftButton.Click += new System.EventHandler(this.LeftButton_Click);
+            // 
+            // UpButton
+            // 
+            this.UpButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.UpButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpButton.Location = new System.Drawing.Point(111, 63);
+            this.UpButton.Name = "UpButton";
+            this.UpButton.Size = new System.Drawing.Size(40, 40);
+            this.UpButton.TabIndex = 1;
+            this.UpButton.Text = "↑";
+            this.UpButton.UseVisualStyleBackColor = true;
+            this.UpButton.Click += new System.EventHandler(this.UpButton_Click);
+            // 
+            // RightButton
+            // 
+            this.RightButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.RightButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RightButton.Location = new System.Drawing.Point(157, 109);
+            this.RightButton.Name = "RightButton";
+            this.RightButton.Size = new System.Drawing.Size(40, 40);
+            this.RightButton.TabIndex = 2;
+            this.RightButton.Text = "→";
+            this.RightButton.UseVisualStyleBackColor = true;
+            this.RightButton.Click += new System.EventHandler(this.RightButton_Click);
+            // 
+            // DownButton
+            // 
+            this.DownButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.DownButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DownButton.Location = new System.Drawing.Point(111, 155);
+            this.DownButton.Name = "DownButton";
+            this.DownButton.Size = new System.Drawing.Size(40, 40);
+            this.DownButton.TabIndex = 3;
+            this.DownButton.Text = "↓";
+            this.DownButton.UseVisualStyleBackColor = true;
+            this.DownButton.Click += new System.EventHandler(this.DownButton_Click);
+            // 
+            // QuestionButton
+            // 
+            this.QuestionButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.QuestionButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.QuestionButton.Location = new System.Drawing.Point(111, 109);
+            this.QuestionButton.Name = "QuestionButton";
+            this.QuestionButton.Size = new System.Drawing.Size(40, 40);
+            this.QuestionButton.TabIndex = 4;
+            this.QuestionButton.Text = "?";
+            this.QuestionButton.UseVisualStyleBackColor = true;
+            this.QuestionButton.Click += new System.EventHandler(this.QuestionButton_Click);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(12, 12);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(83, 43);
+            this.ConnectButton.TabIndex = 5;
+            this.ConnectButton.Text = "Connect to \r\ncontroller";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // TeleBot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.QuestionButton);
+            this.Controls.Add(this.DownButton);
+            this.Controls.Add(this.RightButton);
+            this.Controls.Add(this.UpButton);
+            this.Controls.Add(this.LeftButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TeleBot";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.TeleBot_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.Button UpButton;
+        private System.Windows.Forms.Button RightButton;
+        private System.Windows.Forms.Button DownButton;
+        private System.Windows.Forms.Button QuestionButton;
+        private System.Windows.Forms.Button ConnectButton;
+    }
+}
+
---
+++ TeleBotGUI/TeleBotGUI/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using System.Text;
+namespace TeleBotGUI
+{
+    public partial class TeleBot : Form
+    {
+        MqttClient client;
+
+        public TeleBot()
+        {
+            InitializeComponent();
+            // create client instance 
+            client = new MqttClient("iot.eclipse.org");
+        }
+
+        private void LeftButton_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+                client.Publish("csci/telerobot/group2", Encoding.UTF8.GetBytes("rotate_anti_clockwise"));
+            else
+                MessageBox.Show("Please connect the controller first");
+        }
+
+        private void UpButton_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+                client.Publish("csci/telerobot/group2", Encoding.UTF8.GetBytes("move_up"));
+            else
+                MessageBox.Show("Please connect the controller first");
+        }
+
+        private void DownButton_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+                client.Publish("csci/telerobot/group2", Encoding.UTF8.GetBytes("move_down"));
+            else
+                MessageBox.Show("Please connect the controller first");
+        }
+
+        private void RightButton_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+                client.Publish("csci/telerobot/group2", Encoding.UTF8.GetBytes("rotate_clockwise"));
+            else
+                MessageBox.Show(@"Please connect the controller first");
+        }
+
+        private void QuestionButton_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+                client.Publish("csci/telerobot/group2", Encoding.UTF8.GetBytes("question_ask_led"));
+            else
+                MessageBox.Show(@"Please connect the controller first");
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+           
+            // register to message received 
+            //client.MqttMsgPublishReceived += new MqttClient.MqttMsgPublishEventHandler(client_MqttMsgPublishReceived);
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+
+            var S1 = new string[] { "csci/telerobot/group2" };
+            // subscribe to the topic "/home/temperature" with QoS 2 
+            client.Subscribe(S1, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+            //client.Publish("csci/telerobot/group2", Encoding.UTF8.GetBytes("hi its done"));
+
+        }
+
+
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+
+            String a = System.Text.Encoding.UTF8.GetString(e.Message);
+            //myUI(System.Text.Encoding.UTF8.GetString(e.Message), MessageTextBox);
+        }
+
+    }
+}
